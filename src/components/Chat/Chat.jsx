b7c0c6c370158fbab92b0a3988a2e8f3679318b8
Abhinay-cqss <abhinay@cqs.in>
2020-11-10 11:40:03 +0530

    socket version change

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,19 +1,29 @@
 /* eslint-disable no-restricted-globals */
 import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import qs from 'query-string'
 import './Chat.css'
 
 let socket;
-const Chat = ()=>{
+const Chat = ({location})=>{
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const ENDPOINT = 'http://localhost:5000'
+    const ENDPOINT = 'localhost:5000/'
     useEffect(() => {
-        const data = qs.parse(location.search);
+        const {name,room} = qs.parse(location.search);
+        setRoom(room);
+        setName(name);
         socket = io(ENDPOINT);
-        console.log(socket);
-    }, [])
+        socket.emit('join',{room,name},(response)=>{
+            console.log(response)
+            if(!response.error){
+                window.alert(response.message)
+                console.log(response.message)
+            } else {
+                console.log(response.error)
+            }
+        })
+    },[ENDPOINT,location.search]);
     return (
         <div>Chat Works!</div>
     )
